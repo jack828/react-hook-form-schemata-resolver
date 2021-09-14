5ba8ae7503f10cd149fe3304555aa11e61366d88
Jack Burgess <jackburgess124@gmail.com>
2021-09-14 15:52:07 +0100

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const schemataResolver = () => {}
+
+module.exports = schemataResolver
---
+++ test/resolver.test.js
@@ -0,0 +1,6 @@
+// const assert = require('assert')
+// const resolver = require('../')
+
+describe('Resolver', () => {
+  it('should resolve')
+})
